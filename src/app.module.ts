bf3e3b0bdf29af75c742b48635cc76c897cd82ec
Tran Tien Minh <trantienminh204@gmail.com>
2026-01-18 09:21:03 +0700

    before setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { appConfig } from './config/app.config'
 import { jwtConfig } from './config/jwt.config'
-import { PrismaModule } from './prisma/prisma.module'
 import { HealthModule } from './modules/health/health.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { validateEnv } from './common/config/validate-env'
@@ -13,7 +12,6 @@ import { validateEnv } from './common/config/validate-env'
       load: [appConfig, jwtConfig],
       validate: validateEnv,
     }),
-    PrismaModule,
     HealthModule,
     AuthModule,
   ],
