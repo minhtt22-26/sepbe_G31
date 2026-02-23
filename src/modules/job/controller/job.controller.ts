c998ba3cb5b602a71d450a7f22bfd190a5bcd8a5
Doankd <he170794kieudinhdoan@gmail.com>
2026-02-23 20:18:04 +0700

    fix  sintax

--- src/modules/job/controller/job.controller.ts
+++ src/modules/job/controller/job.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Post, Query } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { JobService } from "../service/job.service";
 import { JobSearchDto } from "../dtos/job.search.request.dto";
 
 @Controller("job")
 export class JobController {
-  constructor(private readonly jobService: JobService) {}
+  constructor(private readonly jobService: JobService) { }
 
   // Search jobs (Elastic)
   //api/job/search?keyword=abc&sectorId=2&workingShift=AFTERNOON&page=1&limit=10
