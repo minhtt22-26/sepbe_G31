ce8ced3c325cd087b96dc31154ba5c27b135995b
Tuan Sang <tuansangsb2k3@gmail.com>
2026-03-22 22:55:12 +0700

    UPDATE: update auth for occupation and sector

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './service/auth.service'
 import { AuthController } from './controller/auth.controller'
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { HelperModule } from 'src/common/helper/helper.module'
 import { AuthUtil } from './utils/auth.utils'
 import { JwtService } from '@nestjs/jwt'
@@ -8,7 +8,10 @@ import { SessionModule } from '../session/session.module'
 import { AuthJwtAccessStrategy } from './guards/jwt/strategies/auth.jwt.access.strategy'
 import { AuthJwtRefreshStrategy } from './guards/jwt/strategies/auth.jwt.refresh.strategy'
 import { AuthSocialGoogleGuard } from './guards/social/auth.social.google.guard'
+import { AuthJwtAccessGuard } from './guards/jwt/auth.jwt.access.guards'
+import { AuthJwtRefreshGuard } from './guards/jwt/auth.jwt.refresh.guards'
 
+@Global()
 @Module({
   imports: [HelperModule, SessionModule],
   controllers: [AuthController],
@@ -19,7 +22,9 @@ import { AuthSocialGoogleGuard } from './guards/social/auth.social.google.guard'
     AuthJwtAccessStrategy,
     AuthJwtRefreshStrategy,
     AuthSocialGoogleGuard,
+    AuthJwtAccessGuard,
+    AuthJwtRefreshGuard,
   ],
-  exports: [AuthService, AuthUtil],
+  exports: [AuthService, AuthUtil, AuthJwtAccessGuard, AuthJwtRefreshGuard],
 })
 export class AuthModule {}
